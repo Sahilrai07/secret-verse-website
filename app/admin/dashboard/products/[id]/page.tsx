555618d0c6fe2863f1bf5044d3d26b400a90cf65
Chirag Singh <chiraaajsingh7600@gmail.com>
2025-10-08 03:58:11 +0530

    done

--- app/admin/dashboard/products/[id]/page.tsx
+++ app/admin/dashboard/products/[id]/page.tsx
@@ -1,23 +1,23 @@
-import EditForm from "@/components/custom/admin/EditForm";
-import { prisma } from "@/lib/prisma";
-import { notFound } from "next/navigation";
+// import EditForm from "@/components/custom/admin/EditForm";
+// import { prisma } from "@/lib/prisma";
+// import { notFound } from "next/navigation";
 
-const getProductDataById = async (productId: string) => {
-  const data = await prisma?.product.findUnique({
-    where: { id: productId },
-  });
+// const getProductDataById = async (productId: string) => {
+//   const data = await prisma?.product.findUnique({
+//     where: { id: productId },
+//   });
 
-  if (!data) return notFound();
-  return data;
-};
+//   if (!data) return notFound();
+//   return data;
+// };
 
-interface PageProps {
-  params: Promise<{ id: string }>;
-}
+// interface PageProps {
+//   params: Promise<{ id: string }>;
+// }
 
-export default async function EditRoute({ params }: PageProps) {
-  const { id } = await params;
-  const productData = await getProductDataById(id);
+// export default async function EditRoute({ params }: PageProps) {
+//   const { id } = await params;
+//   const productData = await getProductDataById(id);
 
-  return <EditForm data={productData} />;
-}
+//   return <EditForm data={productData} />;
+// }
